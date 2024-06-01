75de1646a82351f76ef67b8d33bff64e7ea7c0fe
Jhon Arley Castañeda Vergara <arleycastaneda22@gmail.com>
2024-05-31 20:25:42 -0500

    RPA AUTOMATIZACION GUI

---
+++ controlDelTeclado.py
@@ -0,0 +1,30 @@
+#Contorl del teclado 
+#Funciones principañes
+import time
+import pyautogui as robot
+#write
+robot.wite("Jhon")
+
+#KeyDown
+robot.keyDown("alt")
+
+#KeyUp
+
+robot.keyUp('alt')
+
+#Press
+time.sleep(2)
+robot.press('alt')
+time.sleep(0.25)
+robot.press('b')
+time.sleep(0.25)
+robot.press('2')
+time.sleep(0.25)
+robot.press('v')
+time.sleep(0.25)
+robot.press('t')
+time.sleep(0.25)
+robot.press('enter')
+
+
+
