df59fca0866d0186927ea840ecb747d9dc590f08
Jhon Arley Castañeda Vergara <arleycastaneda22@gmail.com>
2024-05-31 20:51:06 -0500

    segundo commit de RPA automatizacion GUI

---
+++ controlDelMouse.py
@@ -0,0 +1,43 @@
+import pyautogui
+import time
+#Contorl del mouse 
+
+#funciones principales
+
+#DisplayMousePosition Me permite saber la posiicon  del mouse a medida que lo muevo 
+
+#Position Me permite saber la posiicon  actual de donde se encuentra el mouse 
+
+#MoveTo Me permite mover el mouse donde desee y darle una duraccion
+
+#click Me permite moverlo y darle la cantidad de clicks que quieran 
+
+#Dragto Me permite mover las pestañas 
+
+#Scroll Me permite hacer scroll 
+
+
+# pyautogui.displayMousePosition();
+
+# x,y = pyautogui.position()
+# print(x)
+# print(y)
+
+ ###################MoveTo#############
+# x,y=1000,300
+# pyautogui.moveTo(x,y, duration=3);
+
+
+##############click###########
+
+# # pyautogui.displayMousePosition()
+# pyautogui.click(1265,16, duration=3)
+
+
+#############Dragto#########
+# time.sleep(1)
+# pyautogui.click(1060,12, duration=2);
+# time.sleep(2)
+# pyautogui.dragTo(48,22,2, button="left")
+
+########scroll#########
